38570712ffce578bd60325b6f00c6997aadbf6e4
rapha1908 <rapha.santi@gmail.com>
2025-08-06 00:16:02 +0100

    feat: enhance authentication page with tabs for sign-in and sign-up forms

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,9 +1,84 @@
-const Authentication = () => {
+import { AppWindowIcon, CodeIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export function Authentication() {
   return (
-    <div>
-      <h1>Authentication</h1>
+    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
+      <Tabs defaultValue="account">
+        <TabsList>
+          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+          <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sign-in">
+          <Card>
+            <CardHeader>
+              <CardTitle>Entrar</CardTitle>
+              <CardDescription>
+                Seja bem vindo novamente e comece a explorar nossos produtos.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" placeholder="youremail@gmail.com" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="password">Senha</Label>
+                <Input id="password" placeholder="Senha" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Entrar</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="sign-up">
+          <Card>
+            <CardHeader>
+              <CardTitle>Criar Conta</CardTitle>
+              <CardDescription>
+                Crie uma nova conta gratuita e comece a explorar nossos
+                produtos.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="name">Nome</Label>
+                <Input id="name" type="text" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="password">Senha</Label>
+                <Input id="password" type="password" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="confirmPassword">Confirme Senha</Label>
+                <Input id="confirmPassword" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Crie sua conta</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
   );
-};
+}
 
 export default Authentication;
