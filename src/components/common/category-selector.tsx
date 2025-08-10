49e441a941c85a29d02232e761a8daf5b1c73021
rapha1908 <rapha.santi@gmail.com>
2025-08-10 01:10:30 +0100

    feat: add category page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,6 +1,7 @@
 import { categoryTable } from "@/db/schema";
 
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 interface CategorySelectorProps {
   categories: (typeof categoryTable.$inferSelect)[];
@@ -15,8 +16,11 @@ const CategorySelector = ({ categories }: CategorySelectorProps) => {
             key={category.id}
             variant="ghost"
             className="rounded-full bg-white text-xs font-semibold"
-          >
-            {category.name}
+          >          
+            <Link href={`/category/${category.slug}`}>
+              {category.name}
+            </Link>
+
           </Button>
         ))}
       </div>
